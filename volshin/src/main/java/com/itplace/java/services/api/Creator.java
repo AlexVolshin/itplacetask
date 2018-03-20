386e21756750972786941fcd150b41acf49086c1
AlexCookie <alex.cookie.73@gmail.com>
2018-03-20 21:53:19 +0400

    second homework

---
+++ volshin/src/main/java/com/itplace/java/services/api/Creator.java
@@ -0,0 +1,9 @@
+package com.itplace.java.services.api;
+
+public interface Creator {
+    /**
+     * Записывает обработанные данные в файл
+     * @param outputFileName имя выходного файла
+     */
+    void write(String outputFileName);
+}
